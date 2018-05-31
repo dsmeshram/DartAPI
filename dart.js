3ec97103c7a57e976cb0cb42188afde23cea7e17
damodar <damodhar.meshram@hurix.com>
2018-05-31 12:00:17 +0530

    ad api file

---
+++ dart.js
@@ -0,0 +1,66 @@
+var dart = function(option){
+  dartapi = this;
+  dartapi.option = option;
+}
+
+dart.prototype.bind = function (element,object) {
+  bindData(element,object);
+};
+
+
+dart.prototype.repeart = function (element,object) {
+  repeart(element,object);
+};
+
+function repeart(domElement,obj){
+
+
+
+  for(var a =0;a<obj.length;a++)
+  {
+
+    var currentNode,
+      ni = document.createNodeIterator(domElement,NodeFilter.SHOW_ELEMENT);
+
+    while(currentNode = ni.nextNode())
+    {
+        if(currentNode.getAttribute("bind") != null){
+          var bind = currentNode.getAttribute("bind").split(":");
+          if(a == 0){
+            var cln = domElement;
+
+            var domAttr = bind[0].trim(); // the attribute on the DOM element
+            var itemAttr = bind[1].trim(); // the attribute the object
+            var values = obj[a][itemAttr];
+            cln[domAttr] = decodeURIComponent(values);
+            currentNode.parentElement.appendChild(cln);
+          }else{
+            var cln = currentNode.cloneNode(true);
+            var domAttr = bind[0].trim(); // the attribute on the DOM element
+            var itemAttr = bind[1].trim(); // the attribute the object
+            var values = obj[a][itemAttr];
+            cln[domAttr] = decodeURIComponent(values);
+            currentNode.parentElement.appendChild(cln);
+          }
+        }
+    }
+
+  }
+}
+
+function bindData(domElement,obj){
+  var currentNode,
+    ni = document.createNodeIterator(domElement,NodeFilter.SHOW_ELEMENT);
+
+  while(currentNode = ni.nextNode())
+  {
+      if(currentNode.getAttribute("bind") != null){
+        domElement = currentNode;
+        var bind = domElement.getAttribute("bind").split(":");
+        var domAttr = bind[0].trim(); // the attribute on the DOM element
+        var itemAttr = bind[1].trim(); // the attribute the object
+        var values = obj[itemAttr];
+        domElement[domAttr] = decodeURIComponent(values);
+      }
+  }
+}
---
+++ exec.js
@@ -0,0 +1,33 @@
+var option = {
+  apikey:'DEMO5445435DF435'
+}
+
+var dartobject = new dart(option);
+
+var user =  {
+  username:'damodhar',
+  password:'password'
+}
+
+dartobject.bind(document.getElementById('login'),user);
+
+var user1 =  {
+  username:'damodhar',
+  password:'password'
+}
+
+var user2 =  {
+  username:'damodhar 1',
+  password:'password 2'
+}
+
+var user3 =  {
+  username:'damodhar 1',
+  password:'password 2'
+}
+var datalist = [];
+datalist.push(user1);
+datalist.push(user2);
+datalist.push(user3);
+
+dartobject.repeart(document.getElementById('repeat'),datalist);
